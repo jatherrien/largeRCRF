0662c9dfc3f97e5cb561dda889a177630042298d
Joel Therrien <joelt@sfu.ca>
2019-07-23 11:18:40 -0700

    Fix bug where if you run tests without first installing the package, tests fail.

--- R/zzz.R
+++ R/zzz.R
@@ -4,7 +4,7 @@
   # it's in the inst folder which won't be present if the package was actually
   # loaded from the R library). Thus with morePaths we make sure it loads up the
   # jar in this situation.
-  .jpackage(pkgname, lib.loc=libname, morePaths="inst/java/largeRCRF-1.0-SNAPSHOT.jar")
+  .jpackage(pkgname, lib.loc=libname, morePaths="inst/java/largeRCRF-library-1.0-SNAPSHOT.jar")
   
 }
 
